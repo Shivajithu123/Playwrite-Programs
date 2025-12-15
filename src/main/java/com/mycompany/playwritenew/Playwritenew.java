9fff966b0628b4000587b9876a05547407e2f4a8
jithushiva123 <99196035+Shivajithu123@users.noreply.github.com>
2025-12-15 09:55:53 +0530

    Update Playwritenew.java
    verify

--- src/main/java/com/mycompany/playwritenew/Playwritenew.java
+++ src/main/java/com/mycompany/playwritenew/Playwritenew.java
@@ -11,6 +11,6 @@ package com.mycompany.playwritenew;
 public class Playwritenew {
 
     public static void main(String[] args) {
-        System.out.println("Hello World python!");
+        System.out.println("Hello World  java,python!");
     }
 }
